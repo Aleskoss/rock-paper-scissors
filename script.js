d36a9ee24c12e60e95bb271160f411745898d922
Aleskoss <187804686+Aleskoss@users.noreply.github.com>
2025-01-06 22:29:26 +0100

    Create a function for human choice

--- script.js
+++ script.js
@@ -5,5 +5,18 @@ function getComputerChoice(){
     let pick = Math.random() * 100
     pick <= 33 ? console.log("rock") : pick <= 66 ? console.log("scissors") : console.log("paper")
 }
+// a function where we ask user to input rock paper or scissors
+// choice will be stored in variable userInput
+// if choice is not rock paper or scissors ask user again for input
 
+function getHumanChoice(){
+    let userInput = prompt("Choose rock paper or scissors")
+    if(userInput.toLowerCase() === "rock" || userInput.toLowerCase() === "paper" || userInput.toLowerCase() ===  "scissors"){
+        return userInput
+    }else{
+        alert("that`s not right input rock paper or scissors") 
+    }
+}
+//
+getHumanChoice()
 getComputerChoice()
