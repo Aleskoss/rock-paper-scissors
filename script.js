d7d4c3be2ea4a2a3aa5691ed1bd6c6115dc61a5e
Aleskoss <187804686+Aleskoss@users.noreply.github.com>
2025-01-07 21:07:03 +0100

    Make the code more readable by deleting old comments

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Write a function that will randomly return a string of either rock, paper or scissors
 // use math.random function to pick a random number
-// if number is lower than 33 its rock if lower that 66 then paper if lower than 100 then scissors
+// if number is lower than 33 its rock if lower than 66 then paper if lower than 100 then scissors
 function getComputerChoice(){
     let pick = Math.random() * 100
     if(pick <= 33){ 
@@ -23,41 +23,19 @@ function getHumanChoice(){
         alert("that`s not right input rock paper or scissors") 
     }
 }
-// create variables that will hold the score
-// create function that takes in returns of function getHumanChoice and getComputerChoice 
-// decide which one wins with if statements
-// increment eather the humanScore or computerScore
-
-// function playRound(humanChoice,computerChoice){
-//     if(computerChoice === "rock" && humanChoice === "scissors"){
-//         console.log("You lose! Rock beats Scissors!")
-//         computerScore++
-//     }else if(computerChoice === "paper" && humanChoice === "rock"){
-//         console.log("You lose! Paper beats Rock!")
-//         computerScore++
-//     }else if(computerChoice === "scissors" && humanChoice === "paper"){
-//         console.log("You lose! Scissors beats Paper!")
-//         computerScore++
-//     }else if(computerChoice === "rock" && humanChoice === "paper"){
-//         console.log("You Win! Paper beats Rock!")
-//         humanScore++
-//     }else if(computerChoice === "paper" && humanChoice === "scissors"){
-//         console.log("You Win! Scissors beats Paper!")
-//         humanScore++
-//     }else if(computerChoice === "scissors" && humanChoice === "rock"){
-//         console.log("You Win! Rock beats Scissors!")
-//         humanScore++
-//     }else{
-//         console.log("It`s a draw!")
-//     }
-// }
 
 
-// function playGame that calls on function playRound untill either human or computer has 5 points
+// function playGame that calls on function playRound 5 times and decides overall winnner
+// after 5 round If computer has more points or human has more points decide a winner
 
 function playGame(){
     let humanScore = 0
     let computerScore = 0   
+    // create variables that will hold the score
+    // create function that takes in returns of function getHumanChoice and getComputerChoice 
+    // decide which one wins with if statements
+    // increment eather the humanScore or computerScore
+
     function playRound(humanChoice,computerChoice){
         if(computerChoice === "rock" && humanChoice === "scissors"){
             computerScore++
@@ -81,11 +59,9 @@ function playGame(){
             console.log("It`s a draw!")
         }
     }
+    for (let i = 0; i < 5; i++) {
         playRound(getHumanChoice(),getComputerChoice())
-        playRound(getHumanChoice(),getComputerChoice())
-        playRound(getHumanChoice(),getComputerChoice())
-        playRound(getHumanChoice(),getComputerChoice())
-        playRound(getHumanChoice(),getComputerChoice())
+      }
 
         if (humanScore > computerScore){
             console.log("You win!")
