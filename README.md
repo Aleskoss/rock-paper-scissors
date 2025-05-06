0b8b56e097a3cfd84076dcd29e65f5be5be57390
Aleskoss <187804686+Aleskoss@users.noreply.github.com>
2025-05-06 22:10:11 +0200

    Create a function playRound

--- README.md
+++ README.md
@@ -5,3 +5,6 @@ Third project from odin project curriculum where i will demonstrate more of what
     if random number = 0 then return rock else if random number = 1 paper else scissors
 2. Write logic for human choice of rock paper scissors
     Get user input through prompt and store it in variable user userChoice
+3. create variables that will hold computer score and human score named computerScore and humanScore initialized with number 0
+4. Write logic to play a single round that will have two parameters userChoice and humanChoice, this function will take in arguments as getComputerChoice and getHumanChoice after a round it will increment the score 
+  if human choice(transferd to lower case so the choice is case insensitive) wins then it increments human score, else computer score 
--- js/script.js
+++ js/script.js
@@ -1,16 +1,34 @@
+let computerScore = 0
+let humanScore = 0
 function getComputerChoice(){
   let randomNumber = Math.floor(Math.random() * 3)
   if(randomNumber === 0){
-    return "Rock"
+    return "rock"
   }else if(randomNumber === 1){
-    return "Paper"
+    return "paper"
   }else if(randomNumber === 2){
-    return "Scissors"
+    return "scissors"
   }
 }
 function getUserChoice(){
   let userChoice = prompt("Rock Paper or Scissors, what will you choose to play?")
   return userChoice
 }
-
-console.log(getUserChoice())
+function playRound(computerChoice, userChoice){
+  let newComputerChoice = computerChoice()
+  let newUserChoice = userChoice()
+  if(newUserChoice.toLowerCase() === "rock" && newComputerChoice === "scissors" 
+  || newUserChoice.toLowerCase() === "paper" && newComputerChoice === "rock" 
+  || newUserChoice.toLowerCase() === "scissors" && newComputerChoice === "paper"){
+    console.log("Human wins")
+    humanScore += 1
+  }else if(!(newUserChoice.toLowerCase() === "rock" && newComputerChoice === "scissors" 
+  || newUserChoice.toLowerCase() === "paper" && newComputerChoice === "rock" 
+  || newUserChoice.toLowerCase() === "scissors" && newComputerChoice === "paper")){
+    console.log("Computer wins")
+    computerScore += 1
+  }else{
+    console.log("It`s a draw")
+  }
+}
+playRound(getComputerChoice,getUserChoice)
